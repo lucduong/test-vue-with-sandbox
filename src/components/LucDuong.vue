fe2c07b88017272e0f7c103a9a7b90135fc63032
Duong Tuan Luc <luc@ltv.vn>
2018-02-13 09:48:34 +0700

    Initial commit

---
+++ src/components/LucDuong.vue
@@ -0,0 +1,29 @@
+<template>
+<p v-if="name">
+  Hello {{name}}
+</p>
+<p v-else>
+  Welcome to Luc Duong component
+</p>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: ""
+    };
+  },
+
+  created() {
+    this.name = "Luc Duong";
+  }
+};
+</script>
+
+<style scoped>
+p {
+  color: #017eba;
+  font-weight: bold;
+}
+</style>
